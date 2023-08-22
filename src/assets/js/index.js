a90234253bdb07ce55601ff77b0e9514bc9e5db6
Gabriel Alves <gabrielalveskos@gmail.com>
2023-08-22 10:14:34 -0300

    curriculo add

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -11,5 +11,10 @@ function addname(){
 }
 addname();
 
+
+function contrucao(){
+    alert("Sistema\nEssa categoria ainda esta sendo desenvolvida, sera disponibilizada na proxima atualização do site\n\nObrigado pela atenção de todos")
+}
+
 //Colocar o baixador de pdf aqui
 //document.location.origin
